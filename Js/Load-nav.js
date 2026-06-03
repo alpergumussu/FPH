08bfecab1fc57fc16be1aa9f75f309dae2dad96f
Alper Gumussu (student) <s1205693@student.windesheim.nl>
2026-06-03 20:57:23 +0200

    nav 404

--- Js/Load-nav.js
+++ Js/Load-nav.js
@@ -1,7 +1,10 @@
 const navContainer = document.getElementById('navigation');
 
 if (navContainer) {
-    fetch('/Models/Shared/Navigation.html')
+    const depth = location.pathname.split('/').filter(Boolean).length - 1;
+    const prefix = depth > 1 ? '../'.repeat(depth - 1) : '';
+
+    fetch(`${prefix}Models/Shared/Navigation.html`)
         .then(r => r.text())
         .then(html => {
             navContainer.innerHTML = html;
