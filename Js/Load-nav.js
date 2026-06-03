6210548352a72849ad592850404a4cd3c3bc27f9
Alper Gumussu (student) <s1205693@student.windesheim.nl>
2026-06-03 21:36:49 +0200

    atempt

--- Js/Load-nav.js
+++ Js/Load-nav.js
@@ -1,9 +1,12 @@
+const pathParts = location.pathname.split('/').filter(Boolean);
+const base = pathParts.length > 1 ? '/' + pathParts[0] : '';
+
 const navContainer = document.getElementById('navigation');
 
 if (navContainer) {
-    fetch('/Models/Shared/Navigation.html')
+    fetch(`${base}/Models/Shared/Navigation.html`)
         .then(r => r.text())
         .then(html => {
-            navContainer.innerHTML = html;
+            navContainer.innerHTML = html.replaceAll('href="/', `href="${base}/`);
         });
 }
