16f5bf4b908bca3370c0eaa7e6e1882c9444f6ef
Alper Gumussu (student) <s1205693@student.windesheim.nl>
2026-06-03 21:31:23 +0200

    attempt 10000

--- Js/Load-nav.js
+++ Js/Load-nav.js
@@ -1,11 +1,7 @@
 const navContainer = document.getElementById('navigation');
 
-const pathParts = location.pathname.split('/').filter(Boolean);
-const repoDepth = location.hostname.endsWith('github.io') ? 1 : 0;
-const basePath = '/' + pathParts.slice(0, repoDepth).join('/') + '/';
-
 if (navContainer) {
-    fetch(basePath + 'Models/Shared/Navigation.html')
+    fetch('/Models/Shared/Navigation.html')
         .then(r => r.text())
         .then(html => {
             navContainer.innerHTML = html;
