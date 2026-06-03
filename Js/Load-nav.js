28fba0f1849e88646ad8e99d5f8d78b7d8aaf3c1
Alper Gumussu (student) <s1205693@student.windesheim.nl>
2026-06-03 21:00:00 +0200

    fix

--- Js/Load-nav.js
+++ Js/Load-nav.js
@@ -1,10 +1,7 @@
 const navContainer = document.getElementById('navigation');
 
 if (navContainer) {
-    const depth = location.pathname.split('/').filter(Boolean).length - 1;
-    const prefix = depth > 1 ? '../'.repeat(depth - 1) : '';
-
-    fetch(`${prefix}Models/Shared/Navigation.html`)
+    fetch('/FPH/Models/Shared/Navigation.html')
         .then(r => r.text())
         .then(html => {
             navContainer.innerHTML = html;
